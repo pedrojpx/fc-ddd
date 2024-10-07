b79ddfce20a6debb8d55d5c9e6a8225c16337ee6
pedrojpx <pjpxavier@gmail.com>
2024-10-07 10:49:14 -0300

    changed customer to an actual, rich ddd entity. Check class notes for business entity vs persistence entity

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,33 +1,43 @@
 class Customer {
     _id: string;
     _name: string;
-    _address: string;
+    _address: string = "";
+    _active: boolean = false;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
     }
 
-    get id(): string {
-        return this._id;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-    
-    get address(): string {
-        return this._address;
-    }
-
-    set name(name: string) {
+    //diferently from a "get name", this denotes the intention for the function instead of simply "following protocol"
+    changeName(name: string) {
+        if (this._name.length === 0) {
+            throw new Error("Name is required")
+        }
         this._name = name;
-    }
-    
-    set address(address: string) {
-        this.address = address
+        this.validate();
     }
 
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
+    }
+
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required")
+        }
+
+        if (this._id.length === 0) {
+            throw new Error("Id is required")
+        }
+    }
 
 }
