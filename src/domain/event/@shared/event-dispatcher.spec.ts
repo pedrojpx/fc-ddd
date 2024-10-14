80becf3f1ed7f6c11b6f2c57fc9a8afdcb512fd7
pedrojpx <pjpxavier@gmail.com>
2024-10-14 10:37:16 -0300

    domain events - created all tests, except notify

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,3 +1,7 @@
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-product-is-created.handler"
+import ProductCreatedEvent from "../product/product-created.event"
+import EventDispatcher from "./event-dispatcher"
+
 describe("Domain events tests", () => {
 
     it("should register an event handler", () => {
@@ -8,8 +12,52 @@ describe("Domain events tests", () => {
         eventDispatcher.register("ProductCreatedEvent", eventHandler)
 
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
-        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1) 
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler) 
 
+    })
+
+    it("should unregister an event handler", () => {
+
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler) 
+
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0) 
+
+    })
+    
+    it("should unregister all event handlers", () => {
+
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler) 
+
+        eventDispatcher.unregisterAll()
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined() 
+
+    })
+
+    it("should notify all event handlers", () => {
+
+        const eventDispatcher = new EventDispatcher()
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler) 
+
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            description: "product description",
+            price: 10.0
+        })
+
+        eventDispatcher.notify(productCreatedEvent)
     })
 
 })
